7fe2acc5934ec30c5d4cceafb8936cf558cfe216
DragonKnightOfBreeze <dk_breeze@qq.com>
2024-10-08 17:20:07 +0800

    0.1 更新

--- src/main/kotlin/icu/windea/ut/toolbox/lang/json/JsonManager.kt
+++ src/main/kotlin/icu/windea/ut/toolbox/lang/json/JsonManager.kt
@@ -2,9 +2,12 @@ package icu.windea.ut.toolbox.lang.json
 
 import com.intellij.json.psi.JsonProperty
 import com.intellij.json.psi.JsonValue
+import com.intellij.openapi.application.runReadAction
 
 object JsonManager {
     fun isPropertyKey(element: JsonValue): Boolean {
-        return element.parent.let { it is JsonProperty && it.nameElement == element }
+        return runReadAction {
+            element.parent.let { it is JsonProperty && it.nameElement == element }
+        }
     }
 }
